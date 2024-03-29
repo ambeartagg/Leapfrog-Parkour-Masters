1514ccb5a4e5e54354d198601e4169776eef2f6a
ambeartagg <117792178+ambeartagg@users.noreply.github.com>
2024-03-29 12:40:29 -0600

    Added Scoreboards

--- Assets/LeapFrogTrigger.cs
+++ Assets/LeapFrogTrigger.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LeapFrogTrigger : MonoBehaviour
 {
+    public TextMeshProUGUI scoreBoard;
+    public TextMeshProUGUI winScreen;
+    public int score;
+    public bool isPlayer1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +27,20 @@ public class LeapFrogTrigger : MonoBehaviour
         if (other.transform.tag == "Player")
         {
            Debug.Log("Score!");
+            score++;
+            if (isPlayer1)
+            {
+                scoreBoard.text = "P2 score:" + score;
+            }
+            else
+            {
+                scoreBoard.text = "P1 score:" + score;
+            }
+            
+            if (score >= 4)
+            {
+                winScreen.text = "Player wins!:)";
+            }
         }
     }
 }
